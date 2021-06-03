14f056424f42e8d8bcbd38c32c2965b2cc458b06
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-03 14:14:02 +0200

    imported reflect-metadata for TypeORM

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 /*------------------------*/
 import { ContactModule } from '../contact/contact.module';
 /*------------------------*/
+import 'reflect-metadata';
 
 @Module({
   imports: [ContactModule],
