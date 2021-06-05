f7a4ea5da3995e520484d0edd0e53e8db609c48f
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-05 10:24:10 +0200

    fuckig postgress works now!

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -9,13 +9,9 @@ import { UserModule } from './user/user.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_URL,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: ['src/user/models/*.ts', './dist/src/entity/*.js'], // <- Here!
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
       synchronize: true,
     }),
     UserModule,
